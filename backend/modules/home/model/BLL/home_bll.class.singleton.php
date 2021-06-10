8151c70e0fdb3988448c2bbe40196f93044e52d6
abmataix5 <mataix.ab@gmail.com>
2021-06-10 13:08:02 +0200

    limpieza

--- backend/modules/home/model/BLL/home_bll.class.singleton.php
+++ backend/modules/home/model/BLL/home_bll.class.singleton.php
@@ -12,7 +12,7 @@
 	    public static function getInstance() {
 			
 	        if (!(self::$_instance instanceof self)){
-				/* echo("entraaa w2"); */
+			
 	            self::$_instance = new self();
 	        }
 	        return self::$_instance;
@@ -21,25 +21,25 @@
 
 		public function obtain_carousel_BLL(){
 		
-			/*  echo ("holas");   */
+		
 			return $this->dao->select_data_carousel($this->db);  
 		}
 
 		public function obtain_categories_BLL(){
 		
-			/*  echo ("holas");   */
+		
 			return $this->dao->select_data_categories($this->db);  
 		}
 
 		public function obtain_morevisited_BLL($arrArgument){
 		
-			/*  echo ("holas");   */
+		
 			return $this->dao->select_data_morevisited($this->db,$arrArgument);  
 		}
 
 		public function obtain_more_groups_BLL(){
 		
-			/*  echo ("holas");   */
+		
 			return $this->dao->select_data_more_groups($this->db);  
 		}
 
